312cc5c92c1469321c3b17edba96e8d0b196bbf8
yonatan-maharat <hpioo56@gmail.com>
2020-03-25 09:46:12 +0200

    paypal

--- src/components/Cart/PayPalButton.js
+++ src/components/Cart/PayPalButton.js
@@ -13,7 +13,7 @@ export default class MyApp extends React.Component {
 
     const onCancel = data => {
       // User pressed "cancel" or close Paypal's popup!
-      console.log("The payment was cancelled!", data);
+      console.log("The payment was cancelled!");
       // You can bind the "data" object's value to your state or props or whatever here, please see below for sample returned data
     };
 
